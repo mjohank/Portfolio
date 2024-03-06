791c8444a1b4d62e94b48c31a68a0d6362ec09f3
mjohank <mjohank@gmail.com>
2024-03-06 17:07:47 +0100

    Added function to toggle menu icon open/close

--- js/utility-scripts/menuToggle.js
+++ js/utility-scripts/menuToggle.js
@@ -1,8 +1,10 @@
+//  CONSTANTS
 const menuToggle = document.querySelector(".menu-checkbox");
-
 const navLinks = document.querySelectorAll(".nav-list .nav-link a");
+const openIcon = document.querySelector(".open-icon");
+const closeIcon = document.querySelector(".close-icon");
 
-//FUNCTION TO UNCHECK THE MENU TOGGLE
+//FUNCTION TO CLOSE MENU ON LINK CLICK
 
 function closeMenu() {
   menuToggle.checked = false;
@@ -11,3 +13,19 @@ function closeMenu() {
 navLinks.forEach((link) => {
   link.addEventListener("click", closeMenu);
 });
+
+// FUNCTION TO TOGGLE MENU ICONS
+function toggleMenuIcons() {
+  if (menuToggle.checked) {
+    openIcon.style.display = "none";
+    closeIcon.style.display = "inline-block";
+  } else {
+    openIcon.style.display = "inline-block";
+    closeIcon.style.display = "none";
+  }
+}
+
+toggleMenuIcons();
+
+// Update icon on change
+menuToggle.addEventListener("change", toggleMenuIcons);
